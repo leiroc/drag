544a5576fddb42aea77c77196029ecd763ffa892
wbh5 <121644750@qq.com>
2016-04-12 16:20:33 +0800

    fixed bug

--- plugins/drag.js
+++ plugins/drag.js
@@ -42,7 +42,7 @@ Drag.prototype.init = function () {
     this.dragEle.style.cssText += 'position:absolute;-webkit-transform: translate3d(0,0,0);-webkit-backface-visibility: hidden;';
     this.dragEle.setAttribute('pos', this.left+'-' + this.top);
 
-    this.opts.ondrag.call(this, {left: this.left,top: this.top});
+    //this.opts.ondrag.call(this, {left: this.left,top: this.top});
     this.checkHit({left: this.left,top: this.top});
 };
 
@@ -66,7 +66,7 @@ Drag.prototype.setEvent = function(){
         self.left = left;
         self.top = top;
 
-        self.opts.ondrag.call(self, {left: left, top: top, offsetX: self.offsetX, type: 'start'});
+        //self.opts.ondrag.call(self, {left: left, top: top, offsetX: self.offsetX, type: 'start'});
         self.checkHit({left: left, top: top, offsetX: self.offsetX, type: 'start'});
 
     };
@@ -83,7 +83,7 @@ Drag.prototype.setEvent = function(){
 
         self.dragEle.style.cssText += '-webkit-transform: translate3d('+self.offsetX+'px,'+self.offsetY+'px,0);';
         self.dragEle.setAttribute('pos', left+'-' + top);
-        self.opts.ondrag.call(self, {left: left, top: top, offsetX: self.offsetX, type: 'move'});
+        //self.opts.ondrag.call(self, {left: left, top: top, offsetX: self.offsetX, type: 'move'});
         self.checkHit({left: left, top: top, offsetX: self.offsetX, type: 'move'});
     };
 
@@ -94,7 +94,7 @@ Drag.prototype.setEvent = function(){
 
         self.dragEle.setAttribute('pos', left+'-' + top);
         self.dragEle.style.cssText += '-webkit-transform: translate3d(0,0,0);left: '+ left + 'px; ' + 'top: ' + top + 'px;right:auto;bottom:auto;';
-        self.opts.ondrag.call(self, {left: left,top: top, offsetX: self.offsetX, type: 'end'});
+        //self.opts.ondrag.call(self, {left: left,top: top, offsetX: self.offsetX, type: 'end'});
         self.checkHit({left: left,top: top, offsetX: self.offsetX, type: 'end'});
 
         self.offsetX = self.offsetY = 0;
@@ -116,7 +116,7 @@ Drag.prototype.reset = function () {
     this.top = this.bTop;
     this.dragEle.setAttribute('pos', this.bLeft+'-' + this.bTop);
     this.dragEle.style.cssText += '-webkit-transform: translate3d(0,0,0);left: '+ this.bLeft + 'px; ' + 'top: ' + this.bTop + 'px;right:auto;bottom:auto;';
-    this.opts.ondrag.call(this, {left: this.left,top: this.top, offsetX: this.offsetX, type: 'start'});
+    //this.opts.ondrag.call(this, {left: this.left,top: this.top, offsetX: this.offsetX, type: 'start'});
     this.checkHit({left: this.left,top: this.top, offsetX: this.offsetX, type: 'start'})
 
 };
@@ -143,6 +143,7 @@ Drag.prototype.checkHit = function (obj) {
             });
         }
     } else {
+        self.opts.ondrag.call(self, obj);
         self.targetEle.style.cssText += 'background-color: #007aff';
     }
 
